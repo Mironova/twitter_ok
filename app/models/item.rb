c30030af778b9bfcebfa076ea31dde513d69466c
Mironova <iradilema@gmail.com>
2014-05-29 16:20:25 +1100

    create controller Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+
+  after_initialize { puts "inizialized" }
+  after_save { puts "saved" }
+  after_create { puts "created" }
+  after_update { puts "updated" }
+  after_destroy { puts "destroyed" }
+
 end
