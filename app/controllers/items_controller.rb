03a09026813a6e54ecd39d82d1d6a97355bfef27
igor <igortr333@gmail.com>
2014-06-02 14:11:52 +1100

    create index action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
     @items = Item.all
     render text: @items.map { |i| "#{i.name}: #{i.price}" }.join('<br/>')
   end
+
+  def create
+    p params
+    render text: "Item created"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TwitterOk::Application.routes.draw do
+  get "items/index"
+  get "items/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
